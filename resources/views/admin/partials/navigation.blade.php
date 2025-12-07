3cabad80285c7fe09796bf2389c67a39fe2ddb27
faidilzahpar <paidilzahpar@gmail.com>
2025-12-07 19:32:12 +0700

    change navigation name

--- resources/views/admin/partials/navigation.blade.php
+++ resources/views/admin/partials/navigation.blade.php
@@ -31,7 +31,7 @@
 
         <a href="#" class="flex items-center space-x-3 p-3 rounded-lg hover:bg-blue-500 transition">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3"/></svg>
-            <span>Verifikasi Pembayaran</span>
+            <span>Pembayaran</span>
         </a>
 
         <a href="#" class="flex items-center space-x-3 p-3 rounded-lg hover:bg-blue-500 transition">
